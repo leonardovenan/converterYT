a9dc22312a3fbc387c51c113ce659c8bde774f2b
leonanrdovenan <leonardovenancio12@gmail.com>
2023-01-21 16:19:42 -0300

    Teste realizado com sucesso

--- converter.py
+++ converter.py
@@ -1,9 +1,11 @@
-from pytube import Youtube
+from pytube import YouTube
 import pytube
 import os
 
 def main():
   link = input('Digite o link do vídeo do YouTube para fazer a conversão: ')
+
+  print(os.name)
 
   if os.name == 'nt':
     path = os.getcwd() + '\\'
@@ -11,14 +13,18 @@ def main():
     path = os.getcwd() + '/'
   
   name = pytube.extract.video_id(link)
-  Youtube(link).streams.filter(only_audio=True).first().download(filename=name)
+  YouTube(link).streams.filter(only_audio=True).first().download(filename=name)
+  print(f'path = {path}')
+  print(f'name = {name}')
   location = path + name + '.mp4'
-  mp3_name = path + name + '.mp3' 
+  mp3_name = path + name + '.mp3'
+  print(f'location = {location}')
+  print(f'mp3 name = {mp3_name}')
 
   if os.name == 'nt':  
-    os.system(f'ren {location} {mp3_name}')
+    os.system('ren {0}{1}'.format(location, mp3_name))
   else:
-    os.system(f'mv {location} {mp3_name}')
+    os.system('mv {0}{1}'.format(location, mp3_name))
 
 if __name__ == '__main__':
   main()
